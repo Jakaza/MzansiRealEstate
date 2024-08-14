3886f884e3f1948802477ca3dd5dfa94c9a152bb
Themba G Chauke <69904835+Jakaza@users.noreply.github.com>
2024-08-14 14:28:34 +0200

    Update auth.route.js

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -3,14 +3,8 @@ import { login, logout, register } from "../controllers/auth.controller.js";
 const router = express.Router();
 
 
-router.post("/register", (req ,res ) =>{
-    res.json({message: "Sucess"});
-});
-router.post("/login", (req ,res ) =>{
-    res.json({message: "Sucess"});
-});
-router.post("/logout", (req ,res ) =>{
-    res.json({message: "Sucess"});
-});
+router.post("/register", register);
+router.post("/login", login);
+router.post("/logout", logout);
 
 export default router;
