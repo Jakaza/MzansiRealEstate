5f463c19649217fb9c1a2d9b958292d6dab07366
Themba G Chauke <69904835+Jakaza@users.noreply.github.com>
2024-08-14 14:28:09 +0200

    Update app.js

--- api/app.js
+++ api/app.js
@@ -1,7 +1,7 @@
 import express from "express";
 // import cors from "cors";
 // import cookieParser from "cookie-parser";
-// import authRoute from "./routes/auth.route.js";
+import authRoute from "./routes/auth.route.js";
 import postRoute from "./routes/post.route.js";
 // import testRoute from "./routes/test.route.js";
 // import userRoute from "./routes/user.route.js";
@@ -15,7 +15,7 @@ app.use(express.json());
 // app.use(cookieParser());
 
 
-// app.use("/api/auth", authRoute);
+app.use("/api/auth", authRoute);
 // app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
 // app.use("/api/test", testRoute);
